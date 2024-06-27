1006755a37c6250c119cb25ea4c605119fbdcd00
Aziza0011 <khakimovaaziza95@gmail.com>
2024-06-27 09:41:26 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+let a = prompt("1-7 gacha son kiriting ");
+let b = a.toLocaleUpperCase();
+switch(b){
+  case"1":
+  alert("monday");
+    break;
+    case"2":
+    alert("tuesday");
+    break;
+    case"3":
+    alert("wednesday");
+    break;
+    case"4":
+    alert("thursday");
+    break;
+    case"5":
+    alert("friday");
+    break;
+    case"6":
+    alert("saturday");
+    break;
+    case"7":
+    alert("sunday");
+    break;
+    default:
+        alert("1 xaftada 7 kun bor");
+    break;
+
+}
